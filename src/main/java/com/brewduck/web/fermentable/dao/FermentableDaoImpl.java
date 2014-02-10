879d3880a32adc01cffa7c6e04376f9dc48a4ea3
hukoru <hukoru@naver.com>
2014-02-10 18:57:07 +0900

    Fermentable Package 추가

--- src/main/java/com/brewduck/web/fermentable/dao/FermentableDaoImpl.java
+++ src/main/java/com/brewduck/web/fermentable/dao/FermentableDaoImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
-@Repository("styleDao")
+@Repository("fermentableDao")
 public class FermentableDaoImpl implements FermentableDao {
     private static final Logger logger = LoggerFactory.getLogger(FermentableDaoImpl.class);
 
