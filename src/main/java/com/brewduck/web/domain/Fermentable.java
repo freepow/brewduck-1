5fba02b3608dcf0d41052f12b0dc833c5ce833bc
JAEGER <freepow@gmail.com>
2014-02-12 11:49:36 +0900

    발효 VO 추가

--- src/main/java/com/brewduck/web/domain/Fermentable.java
+++ src/main/java/com/brewduck/web/domain/Fermentable.java
@@ -6,6 +6,11 @@ import org.apache.commons.lang.builder.ToStringStyle;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * 발효 Value Object
+ *
+ * 신재근, 2014-02-12
+ */
 public class Fermentable implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -23,7 +28,7 @@ public class Fermentable implements Serializable {
     private Double yield;
     // 맥주의 색상 (SRM)
     private Double color;
-    //끓인 후 추가되는 것의 유무 (Default : "FALSE")
+    // 끓인 후 추가되는 것의 유무 (Default : "FALSE")
     private Boolean addAfterBoil;
     // 원산지 국가 및 장소
     private String orgin;
@@ -41,12 +46,12 @@ public class Fermentable implements Serializable {
     private Double protein;
     // 맥주 최대 배치의 권장하는 비율 ("%")
     private Double maxInBatch;
-    //추천 당화
+    // 추천 당화
     private Boolean recommend_mash;
     /*
-        홉추출물에만 적용됨 -
         1갤런의 물에 넣은 추출물 1파운드당 IBU의 예상값.
-        IBU로 변환하려면 이 숫자를 "amount "(파운드)로 곱한 뒤 배치내 갤런값으로 나누시오.
+        홉 추출물에만 적용됨.
+        IBU로 변환하려면 이 숫자를 "amount"(파운드)로 곱한 뒤 배치내 갤런값으로 나누시오.
         60분 끓이는 것을 가정으로 함.
         추출물 타입에만 적용될 수 있고 다른경우에 이 값은 무시됨.
     */
@@ -56,6 +61,373 @@ public class Fermentable implements Serializable {
     // 수정 날짜
     private Date updateDt;
 
+
+    /**
+     * 발효 한글명
+     *
+     * @return 발효 한글명
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 발효 한글명
+     *
+     * @param name 발효 한글명
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 발효 영문명
+     *
+     * @return 발효 영문명
+     */
+    public String getKoreanName() {
+        return koreanName;
+    }
+
+    /**
+     * 발효 영문명
+     *
+     * @param koreanName 발효 영문명
+     */
+    public void setKoreanName(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    /**
+     * 발효 버전
+     * Fix : "1"
+     *
+     * @return 발효 버전
+     */
+    public Integer getVersion() {
+        return version;
+    }
+
+    /**
+     * 발효 버전
+     *
+     * @param version 발효 버전
+     */
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    /**
+     * 발효 종류
+     * ("Grain", "Sugar", "Extract", "Dry Extract" or "Adjunct")
+     *
+     * @return 발효 종류
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * 발효 종류
+     *
+     * @param type 발효 종류
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * 발효의 양
+     * (발효, 추출 또는 설탕의 무게 : "Kg")
+     *
+     * @return 발효의 양
+     */
+    public Double getAmount() {
+        return amount;
+    }
+
+    /**
+     * 발효의 양
+     *
+     * @param amount 발효의 양
+     */
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * 발효 수율
+     * (중량 원료의 수율 : "%")
+     *
+     * @return 발효 수율
+     */
+    public Double getYield() {
+        return yield;
+    }
+
+    /**
+     * 발효 수율
+     *
+     * @param yield 발효 수율
+     */
+    public void setYield(Double yield) {
+        this.yield = yield;
+    }
+
+    /**
+     * 맥주의 색상 (SRM)
+     *
+     * @return 맥주의 색상
+     */
+    public Double getColor() {
+        return color;
+    }
+
+    /**
+     * 맥주의 색상
+     *
+     * @param color 맥주의 색상
+     */
+    public void setColor(Double color) {
+        this.color = color;
+    }
+
+    /**
+     * 끓인 후 추가되는 것의 유무
+     * (Default : "FALSE")
+     *
+     * @return 끓인 후 추가되는 것의 유무
+     */
+    public Boolean getAddAfterBoil() {
+        return addAfterBoil;
+    }
+
+    /**
+     * 끓인 후 추가되는 것의 유무
+     * (Default : "FALSE")
+     *
+     * @param addAfterBoil 끓인 후 추가되는 것의 유무
+     */
+    public void setAddAfterBoil(Boolean addAfterBoil) {
+        this.addAfterBoil = addAfterBoil;
+    }
+
+    /**
+     * 원산지 국가 및 장소
+     *
+     * @return 원산지 국가 및 장소
+     */
+    public String getOrgin() {
+        return orgin;
+    }
+
+    /**
+     * 원산지 국가 및 장소
+     *
+     * @param orgin 원산지 국가 및 장소
+     */
+    public void setOrgin(String orgin) {
+        this.orgin = orgin;
+    }
+
+    /**
+     * 공급 업체
+     * (곡물 / 추출 / 설탕의 공급 업체)
+     *
+     * @return 공급 업체
+     */
+    public String getSupplier() {
+        return supplier;
+    }
+
+    /**
+     * 공급 업체
+     *
+     * @param supplier 공급 업체
+     */
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    /**
+     * 발효 주의사항(메모)
+     *
+     * @return 발효 주의사항(메모)
+     */
+    public String getNotes() {
+        return notes;
+    }
+
+    /**
+     * 발효 주의사항(메모)
+     *
+     * @param notes 발효 주의사항(메모)
+     */
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    /**
+     * 거친 곡물 생산량 및 미세 입자의 수율 사이의 비율 차이
+     *
+     * @return 거친 곡물 생산량 및 미세 입자의 수율 사이의 비율 차이
+     */
+    public Double getCoarseFineDiff() {
+        return coarseFineDiff;
+    }
+
+    /**
+     * 거친 곡물 생산량 및 미세 입자의 수율 사이의 비율 차이
+     *
+     * @param coarseFineDiff 거친 곡물 생산량 및 미세 입자의 수율 사이의 비율 차이
+     */
+    public void setCoarseFineDiff(Double coarseFineDiff) {
+        this.coarseFineDiff = coarseFineDiff;
+    }
+
+    /**
+     * 곡물의 비율 습기
+     *
+     * @return 곡물의 비율 습기
+     */
+    public Double getMoisture() {
+        return moisture;
+    }
+
+    /**
+     * 곡물의 비율 습기
+     *
+     * @param moisture 곡물의 비율 습기
+     */
+    public void setMoisture(Double moisture) {
+        this.moisture = moisture;
+    }
+
+    /**
+     * 측정된 곡물의 당화 힘
+     *
+     * @return 측정된 곡물의 당화 힘
+     */
+    public Double getDiastaticPower() {
+        return diastaticPower;
+    }
+
+    /**
+     * 측정된 곡물의 당화 힘
+     *
+     * @param diastaticPower 측정된 곡물의 당화 힘
+     */
+    public void setDiastaticPower(Double diastaticPower) {
+        this.diastaticPower = diastaticPower;
+    }
+
+    /**
+     * 곡물의 단백질 ("%")
+     *
+     * @return 곡물의 단백질
+     */
+    public Double getProtein() {
+        return protein;
+    }
+
+    /**
+     * 곡물의 단백질
+     *
+     * @param protein 곡물의 단백질
+     */
+    public void setProtein(Double protein) {
+        this.protein = protein;
+    }
+
+    /**
+     * 맥주 최대 배치의 권장하는 비율 ("%")
+     *
+     * @return 맥주 최대 배치의 권장하는 비율
+     */
+    public Double getMaxInBatch() {
+        return maxInBatch;
+    }
+
+    /**
+     * 맥주 최대 배치의 권장하는 비율
+     *
+     * @param maxInBatch 맥주 최대 배치의 권장하는 비율
+     */
+    public void setMaxInBatch(Double maxInBatch) {
+        this.maxInBatch = maxInBatch;
+    }
+
+    /**
+     * 발효 추천 당화
+     *
+     * @return 발효 추천 당화
+     */
+    public Boolean getRecommend_mash() {
+        return recommend_mash;
+    }
+
+    /**
+     * 발효 추천 당화
+     *
+     * @param recommend_mash 발효 추천 당화
+     */
+    public void setRecommend_mash(Boolean recommend_mash) {
+        this.recommend_mash = recommend_mash;
+    }
+
+    /**
+     * 1갤런의 물에 넣은 추출물 1파운드당 IBU의 예상값
+     *
+     * @return IBU의 예상값
+     */
+    public Double getIbuGalPerLb() {
+        return ibuGalPerLb;
+    }
+
+    /**
+     * 1갤런의 물에 넣은 추출물 1파운드당 IBU의 예상값
+     *
+     * @param ibuGalPerLb IBU의 예상값
+     */
+    public void setIbuGalPerLb(Double ibuGalPerLb) {
+        this.ibuGalPerLb = ibuGalPerLb;
+    }
+
+    /**
+     * 수정자 아이디
+     *
+     * @return 수정자 아이디
+     */
+    public String getUpdateId() {
+        return updateId;
+    }
+
+    /**
+     * 수정자 아이디
+     *
+     * @param updateId 수정자 아이디
+     */
+    public void setUpdateId(String updateId) {
+        this.updateId = updateId;
+    }
+
+    /**
+     * 수정 날짜
+     *
+     * @return 수정 날짜
+     */
+    public Date getUpdateDt() {
+        return updateDt;
+    }
+
+    /**
+     * 수정 날짜
+     *
+     * @param updateDt 수정 날짜
+     */
+    public void setUpdateDt(Date updateDt) {
+        this.updateDt = updateDt;
+    }
 
     @Override
     public String toString() {
